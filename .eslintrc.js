0161cbb007e352e9accb3c04e735c2f6c25f0cbd
jungminji <wjdalswl0215@gmail.com>
2025-04-16 03:43:54 +0900

    [script] prettier & eslint

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+  },
+};
