908638cb2da05448f49bbd44cbb848dd37bf9702
jungminji <wjdalswl0215@gmail.com>
2025-04-21 22:31:49 +0900

    [feat] header 에 필요한 링크 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,20 @@
+import { Link } from 'react-router';
+
 export default function Header() {
-  return <header className="mx-auto max-w-3xl p-4">Header</header>;
+  return (
+    <header className="wrapper bg-gray-100">
+      <div className="flex justify-between">
+        <h2>심플앱</h2>
+        <ul className="flex gap-4">
+          <li>
+            <Link to="/signin">로그인</Link>
+          </li>
+          <li>
+            <Link to="/signup">회원가입</Link>
+          </li>
+          <button>로그아웃</button>
+        </ul>
+      </div>
+    </header>
+  );
 }
