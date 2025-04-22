40966b5c36314849199e5fe2ecdc3b03a55be8bb
jungminji <wjdalswl0215@gmail.com>
2025-04-23 00:04:07 +0900

    [feat](post) : post 등록 기능

--- src/api/post.ts
+++ src/api/post.ts
@@ -3,6 +3,33 @@ import { Post } from '../types/todo.ts';
 export const getPosts = async (): Promise<Post[]> => {
   const response = await fetch(`${import.meta.env.VITE_BASE_URL}/api/posts`);
 
+  if (!response.ok) {
+    const { message } = await response.json();
+    throw new Error(message || '알 수 없는 오류가 발생했습니다.');
+  }
+
   const res = await response.json();
   return res.posts;
+};
+
+export const createPost = async ({
+  content,
+  userId,
+}: {
+  content: string;
+  userId: number;
+}): Promise<Post> => {
+  const response = await fetch(`${import.meta.env.VITE_BASE_URL}/api/posts`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ content, authorId: userId }),
+  });
+
+  if (!response.ok) {
+    const { message } = await response.json();
+    throw new Error(message || '알 수 없는 오류가 발생했습니다.');
+  }
+
+  const data = await response.json();
+  return data.post;
 };
