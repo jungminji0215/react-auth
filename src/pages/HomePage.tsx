4b7aced8f6f5f6814f8c9b90921359159147b3ac
jungminji <wjdalswl0215@gmail.com>
2025-04-24 00:12:09 +0900

    [feat](auth) : auth 관련 페이지 생성

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,16 @@
 import PageContainer from '../components/PageContainer.tsx';
+import { Link } from 'react-router';
 
 export default function HomePage() {
   return (
     <PageContainer>
-      <section>Auth </section>
+      <section>
+        <Link to="/auth">
+          <div className="bg-yellow-500 p-5 rounded-lg w-1/2">
+            🔐 로그인 기능 보기
+          </div>
+        </Link>
+      </section>
     </PageContainer>
   );
 }
--- src/pages/auth/SignInPage.tsx
+++ src/pages/auth/SignInPage.tsx
@@ -1,5 +1,21 @@
-import PageContainer from '../../components/PageContainer.tsx';
-
 export default function SignInPage() {
-  return <PageContainer>SignInPage</PageContainer>;
+  return (
+    <div className="flex justify-center items-center min-h-screen p-4">
+      <form className="flex flex-col gap-2 w-full max-w-sm">
+        <input
+          type="email"
+          placeholder="이메일"
+          className="border border-gray-300 rounded-md p-2"
+        />
+        <input
+          type="password"
+          placeholder="비밀번호"
+          className="border border-gray-300 rounded-md p-2"
+        />
+        <button type="submit" className="bg-yellow-500 rounded-md p-2">
+          로그인
+        </button>
+      </form>
+    </div>
+  );
 }
